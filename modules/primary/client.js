6eb0dd5e962111ede7bfc9cc40109ff3688cd1e1
GabrielDGamer12 <gabrielrleite2011@gmail.com>
2022-12-18 02:53:17 -0300

    First's Files

---
+++ modules/primary/client.js
@@ -0,0 +1,18 @@
+const { Client } = require("./dependencies")
+
+const client = new Client({
+    messageCacheLifetime: 60,
+    fetchAllMembers: false,
+    messageCacheMaxSize: 10,
+    restTimeOffset: 0,
+    restWsBridgetimeout: 100,
+    shards: "auto",
+    allowedMentions: {
+        parse: ["roles", "users", "everyone"],
+        repliedUser: true,
+    },
+    partials: ["MESSAGE", "CHANNEL", "REACTION"],
+    intents: 131071,
+});
+
+module.exports = { client }
